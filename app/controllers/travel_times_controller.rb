ffaac93f1468239bfb8d0b2b452222e77a987f34
matthias haefeli <haefeli.matthias@gmail.com>
2020-07-10 14:21:03 -0600

    refactoring

--- app/controllers/travel_times_controller.rb
+++ app/controllers/travel_times_controller.rb
@@ -10,25 +10,28 @@ class TravelTimesController < ApplicationController
   end
 
   def create
+
+    # create connection and call distance api
     uri = URI('https://maps.googleapis.com/maps/api/distancematrix/json?')
     parameters = { units: 'imperial',
                    origins: params[:start_point],
                    destinations: params[:end_point],
                    key: Rails.application.credentials.google[:key] }
 
+    # if selected something else than driving add more params to the api call
     if params[:transit_mode] != 'driving'
       parameters[:mode] = 'transit'
       parameters[:transit_mode] = params[:transit_mode]
     end
 
     uri.query = URI.encode_www_form(parameters)
-
     response = Net::HTTP.get_response(uri)
     response_hash = JSON.parse(response.body, :symbolize_names => true)
 
     if response_hash[:status] == 'INVALID_REQUEST' || nested_hash_value(nested_hash_value(response_hash, :elements), :status) == 'ZERO_RESULTS'
-      render json: { error: "Can't find any results with this data!" }
+      render json: { error: 'I cannot find any routes with the given data' }
     else
+      # create TraveTime object and store it to the db
       travel_time = TravelTime.new(start_point: response_hash[:origin_addresses].join,
                                    end_point: response_hash[:destination_addresses].join,
                                    distance: nested_hash_value(nested_hash_value(response_hash, :distance), :text),
@@ -47,6 +50,7 @@ class TravelTimesController < ApplicationController
 
   private
 
+  # find nested key and value in hash
   def nested_hash_value(obj,key)
     if obj.respond_to?(:key?) && obj.key?(key)
       obj[key]
